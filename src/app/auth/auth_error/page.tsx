46a0942b7268ab17ab6bb180dc9f074b0d1bf564
ameer <ameer@hancod.com>
2025-07-11 14:47:54 +0530

    fixed tab and sale_view loading

--- src/app/auth/auth_error/page.tsx
+++ src/app/auth/auth_error/page.tsx
@@ -7,38 +7,3 @@ export default function AuthErrorPage() {
     </div>
   );
 }
-
-
-// "use client";
-// import { useEffect } from "react";
-// import { useSearchParams } from "next/navigation";
-// import { toastWithTimeout, ToastVariant } from "@/hooks/use-toast";
-
-// export default function AuthErrorPage() {
-//   const searchParams = useSearchParams();
-
-//   useEffect(() => {
-  
-//     const error = searchParams.get("error");
-//     const errorDescription = searchParams.get("error_description");
-
-//     if (error) {
-//       let message = "There was a problem logging you in. Please try again.";
-//       if (error === "access_denied" && errorDescription?.includes("expired")) {
-//         message = "Your login link has expired. Please request a new one.";
-//       } else if (errorDescription) {
-//         message = errorDescription;
-//       }
-//       toastWithTimeout(ToastVariant.Destructive, message);
-//     }
-//   }, [searchParams]);
-
-//   return (
-//     <div className="text-center p-10">
-//       <h1 className="text-2xl font-bold">Authentication Error</h1>
-//       <p className="text-gray-600 mt-2">
-//         There was a problem logging you in. Please try again.
-//       </p>
-//     </div>
-//   );
-// }
