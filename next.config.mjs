011c20bf25173dc4ab43b3fa693f0d32bf92f215
ameer <ameer@hancod.com>
2026-02-05 15:37:17 +0530

    fix - login page route incorrect lint 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     images: {
-//         domains: [
-//             'hebbkx1anhila5yf.public.blob.vercel-storage.com',
-//             'iqwgvylkgjaqitnqjldp.supabase.co',
-//             'awfbsiftpwpiczdxlmig.supabase.co',
-//             '127.0.0.1:54321',
-//             'localhost'
-//         ],
-//     },
-//     eslint: {
-//         ignoreDuringBuilds: true,
-//     }
-// };
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
