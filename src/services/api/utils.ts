c4294c4ec9a7ea7608b06fcc9e8601a5b9317414
Anshad-KT <anshadkt78@gmail.com>
2026-02-28 11:59:53 +0530

    feat: fixed

--- src/services/api/utils.ts
+++ src/services/api/utils.ts
@@ -57,7 +57,7 @@ const isServer = typeof window === 'undefined';
         // Create Supabase client
        
         
-        this.supabase = createClient("https://api.duxbe.app", this.supabaseKey);
+        this.supabase = createClient("https://duxbe.jiobase.com", this.supabaseKey);
 
         return this;
     }
